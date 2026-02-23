4bf8e5f0c6a4ba019ffe44a59ff04bd244edaa48
kranpant <110833432+kranpant@users.noreply.github.com>
2026-02-23 11:16:07 +0530

    Update start.py

--- anony/modules/start.py
+++ anony/modules/start.py
@@ -6,7 +6,7 @@ from anony import app, buttons, db
 @app.on_message(filters.command(["start"]) & filters.private)
 async def f_start(_, m: types.Message):
     await m.reply_text(
-        text=f"Hey {m.from_user.first_name},\n\nThis is {app.mention},\nAn open source session generator bot.",
+        text=f"ʜᴇʏ {m.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {app.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
         reply_markup=buttons.start_key(),
     )
     await db.add_user(m.from_user.id)
